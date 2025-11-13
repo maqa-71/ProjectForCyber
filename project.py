f4f2c7fdc48005e915a5d878b603424dfd235309
maqa-71 <mehemmedismayilov989@gmail.com>
2025-11-13 11:25:44 +0400

    renamed

---
+++ project.py
@@ -0,0 +1,7 @@
+14.	from selenium import webdriver
+15.	from selenium.webdriver.common.by import By
+16.	from selenium.webdriver.chrome.service import Service
+17.	from selenium.webdriver.chrome.options import Options
+18.	from selenium.webdriver.support.ui import WebDriverWait
+19.	from selenium.webdriver.support import expected_conditions as EC
+20.	import time
