301671211bc5fbe4cee6a1b2ef16b06b806fc749
maqa-71 <mehemmedismayilov989@gmail.com>
2025-11-13 10:28:52 +0400

    done

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import json
 with open("info.json", "r", encoding="utf-8") as file:
     data = json.load(file)
 
-# Выводим данные в читаемом виде
+
 print("Данные из файла:")
 print(json.dumps(data, indent=4, ensure_ascii=False))
